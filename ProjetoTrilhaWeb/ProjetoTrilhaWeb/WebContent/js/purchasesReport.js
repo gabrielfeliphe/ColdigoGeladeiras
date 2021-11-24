99eb76fdc9bea7e99b71f3c38c499cd021483d9f
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2021-11-23 22:04:10 -0300

    OT 04
    falta implementar js da ot

--- ProjetoTrilhaWeb/ProjetoTrilhaWeb/WebContent/js/purchasesReport.js
+++ ProjetoTrilhaWeb/ProjetoTrilhaWeb/WebContent/js/purchasesReport.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
 			type:"GET",
 			url: COLDIGO.PATH+"compra/relatorio",
 			success: function (dados){
-				console.log(dados);
+				dados = JSON.parse(dados);
+				$("#listaCompras").html(COLDIGO.relatorioCompra.exibir(dados));
 			},
 			error: function(info){
 				COLDIGO.exibirAviso("Erro ao consultar as compras: "+ info.status +" - "+info.statusText)
